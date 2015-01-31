1e96d86b7b20224e3b848ff8e63cb56534ff179a
Lucas Zeer-Wanklyn <lucas.zeer@gmail.com>
2015-01-31 14:59:47 -0500

    Commented out file validations

--- app/models/video_storage.rb
+++ app/models/video_storage.rb
@@ -1,9 +1,9 @@
 class VideoStorage < ActiveRecord::Base
 	has_attached_file :file
 	has_attached_file :thumbnail
-  validates_attachment_content_type :file,
-    :content_type => ['video/quicktime', 'video/mp4', 
-    									'video/mpeg', 'video/x-msvideo', 'video/x-sgi-movie']
+  # validates_attachment_content_type :file,
+  #   :content_type => ['video/quicktime', 'video/mp4', 
+  #   									'video/mpeg', 'video/x-msvideo', 'video/x-sgi-movie']
   validates_attachment_content_type :thumbnail,
     :content_type => ['image/jpeg']
     
